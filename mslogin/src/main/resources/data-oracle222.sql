600fba48e71bdb662d0779c981c379ea6fa00743
MostafaMotaharinia <eng.motahari@gmail.com>
2020-10-26 12:06:44 +0330

    add insert query for login test

--- mslogin/src/main/resources/data-oracle222.sql
+++ mslogin/src/main/resources/data-oracle222.sql
@@ -1,11 +1,11 @@
--- INSERT INTO oauth_client_details
--- 	(client_id, client_secret, scope, authorized_grant_types,
--- 	web_server_redirect_uri, authorities, access_token_validity,
--- 	refresh_token_validity, additional_information, autoapprove)
--- VALUES
--- 	('web-client', '{bcrypt}$2a$10$gPhlXZfms0EpNHX0.HHptOhoFD1AoxSr/yUIdTqA8vtjeP4zi0DDu', 'READ,WRITE, TRUST',
--- 	'authorization_code,password,refresh_token,implicit', null, 'ROLE_CLIENT, ROLE_TRUSTED_CLIENT', 60*60*1, 24*60*60*1, null, 'true');
---
+INSERT INTO oauth_client_details
+	(client_id, client_secret, scope, authorized_grant_types,
+	web_server_redirect_uri, authorities, access_token_validity,
+	refresh_token_validity, additional_information, autoapprove)
+VALUES
+	('web-client', '{bcrypt}$2a$10$gPhlXZfms0EpNHX0.HHptOhoFD1AoxSr/yUIdTqA8vtjeP4zi0DDu', 'READ,WRITE, TRUST',
+	'authorization_code,password,refresh_token,implicit', null, 'ROLE_CLIENT, ROLE_TRUSTED_CLIENT', 60*60*1, 24*60*60*1, null, 'true');
 
 
--- INSERT INTO adminuser (username, password, firstname,lastname) VALUES ('eng.motahari@gmail.com', '{bcrypt}$2a$10$eMFS.AIypqHhOcyVsypqj.o3kwRCnwru0N5ddVDUM/7ECXgKGBI5O', 'mostafa','motaharinia');
+
+INSERT INTO adminuser (username, password, firstname,lastname) VALUES ('eng.motahari@gmail.com', '{bcrypt}$2a$10$eMFS.AIypqHhOcyVsypqj.o3kwRCnwru0N5ddVDUM/7ECXgKGBI5O', 'mostafa','motaharinia');
