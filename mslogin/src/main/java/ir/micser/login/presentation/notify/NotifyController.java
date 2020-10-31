4decdcbdedcf7edff704327cec5f4133f30f701d
Maryam <mm.azish.326@gmail.com>
2020-10-31 16:09:22 +0330

    اضافه کردن دیپندنسی درpomپروژه

--- mslogin/src/main/java/ir/micser/login/presentation/notify/NotifyController.java
+++ mslogin/src/main/java/ir/micser/login/presentation/notify/NotifyController.java
@@ -1,10 +1,7 @@
 package ir.micser.login.presentation.notify;
 
-import com.motaharinia.msutility.customexception.BusinessException;
 import io.leangen.graphql.annotations.GraphQLSubscription;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
-import ir.micser.login.business.service.BusinessExceptionEnum;
-import ir.micser.login.business.service.loguploadedfile.LogUploadedFileServiceImpl;
 import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -17,9 +14,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class NotifyController {
     @GraphQLSubscription(name = "briefMesseages")
     public Publisher<NotifyModel> briefMesseages(Integer userId) {
-        if(userId != 2){
-            throw new BusinessException(LogUploadedFileServiceImpl.class, BusinessExceptionEnum.INVALID_ID, "userId:" + userId);
-        }
         AtomicInteger count = new AtomicInteger(0);
         return Flux.interval(Duration.ofSeconds(2)) .map(num -> new NotifyModel(count.incrementAndGet()));
     }
