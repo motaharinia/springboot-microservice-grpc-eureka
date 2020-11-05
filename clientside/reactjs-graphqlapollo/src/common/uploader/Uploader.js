720ef100e39c64beb5a9aec7ee44507dbf158d9f
ali masoomzadeh <eng.masoomzadeh@gmail.com>
2020-11-05 20:34:25 +0330

    update Uploader.js

--- clientside/reactjs-graphqlapollo/src/common/uploader/Uploader.js
+++ clientside/reactjs-graphqlapollo/src/common/uploader/Uploader.js
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from "react";
-import FineUploaderTraditional from "fine-uploader-wrappers";
+import React, {useEffect, useRef, useState} from "react";
 import Gallery from "react-fine-uploader";
 import "react-fine-uploader/gallery/gallery.css";
 
@@ -9,150 +8,22 @@ import Modal from "@material-ui/core/Modal";
 import CloseIcon from "@material-ui/icons/Close";
 
 
-import {changeActionEnum, typeEnum, statusEnum} from './UploaderData'
+import {getNewUploader, statusEnum, typeEnum} from './UploaderData'
 import {UploaderFileView} from './UploaderFileView'
 import {useStyles} from "../Styles";
 
 
 export default function Uploader(props) {
 
+
     //تعریف متغیر استایل
     const classes = useStyles();
 
 
-    //وضعیتها
-    //"SUBMIT": وقتی که یک فایل برای اضافه شدن به آپلودر انتخاب میشود
-    //"SUBMITTED": وقتی که یک فایل به آپلودر اضافه شود
-    //"SUBMIT_DELETE": وقتی که یک فایل برای حذف از آپلودر انتخاب شود
-    //"RESUME": وقتی که ادامه آپلود یک فایل در آپلودر انجام شود
-    //"MANUAL_RETRY": وقتی که ادامه آپلود یک فایل در آپلودر به صورت دستی توسط کاربر انجام شود
-    //"AUTO_RETRY": وقتی که ادامه آپلود یک فایل در آپلودر به صورت اتوماتیک انجام شود
-    //"CANCEL": وقتی که یک فایل در آپلودر کنسل شود
-    //"COMPLETE": وقتی که یکی از آپلود فایلها کامل گردد
-    const onStatusChange = (id, oldStatus, newStatus, openModal) => {
-        const {uploader} = fileData;
-        uploader.methods.getUploads().map(function (obj, index) {
-            let fileObject = JSON.parse(JSON.stringify(obj));
-            if (fileObject.id === id) {
-                fileObject["fileData"] = uploader.methods.getFile(id);
-            }
-            if (fileObject.status !== statusEnum.DELETED) {
-                fileData["objectSelectedList"].push(fileObject)
-            }
-        });
-        if (openModal !== undefined && openModal !== null) {
-            fileData["openModal"] = openModal;
-        }
-        setFileData({
-            ...fileData
-        });
-    };
-
-    const onSubmit = (id, fileName, fileContainer) => {
-        setUploader(fileData);
-        return true;
-    };
-
-
-    let validationObject = {};
-    if (props.validationExtensionList !== undefined) {
-        validationObject["allowedExtensions"] = props.validationExtensionList;
-    }
-    if (props.validationSizeLimit !== undefined) {
-        validationObject["sizeLimit"] = props.validationSizeLimit;
-    }
-    if (props.type === typeEnum.SINGLE) {
-        validationObject["itemLimit"] = 1;
-    } else {
-        if (props.validationItemLimit !== undefined) {
-            validationObject["itemLimit"] = props.validationItemLimit;
-        }
-    }
-
-    // validationObject["acceptFiles"]="image/png, image/jpeg";
-    let fileNameList = [];
-    let linitialStateUploader = new FineUploaderTraditional({
-        options: {
-            chunking: {
-                enabled: true,
-                mandatory: true,
-                concurrent: {
-                    enabled: false
-                },
-                partSize: 2 * 1024 * 1024  //2mb
-            },
-            deleteFile: {
-                method: "POST",
-                enabled: true,
-                endpoint: "/fso/deleteUploadedFile"
-            },
-            request: {
-                customHeaders: {
-                    "Authorization": "Basic 999jaGFkbWluOkNieWxjZTY3"
-                },
-                endpoint: "http://localhost:8082/fso/upload/" + props.subSystem + "/" + props.entity + "/fine"
-            },
-            cors: {
-                autoUpload: false,
-                //all requests are expected to be cross-domain requests
-                expected: true,
-                //if you want cookies to be sent along with the request
-                sendCredentials: true,
-                maxConnections: 1
-            },
-            resume: {
-                enabled: true
-            },
-            retry: {
-                enableAuto: false,
-                maxAutoAttempts: 2,
-            },
-            validation: validationObject,
-            text: {
-                sizeSymbols: ["کیلوبایت", "مگابایت", "گیگابایت", "ترابایت", "PB", "EB"]
-            },
-            messages: {
-                emptyError: "فایل خالی است",
-                maxHeightImageError: "ارتفاع تصویر زیاد است",
-                maxWidthImageError: "عرض تصویر زیاد است",
-                minHeightImageError: "ارتفاع تصویر کم است",
-                minWidthImageError: "عرض تصویر کم است",
-                minSizeError: "حجم فایل کم است. حداقل حجم فایل {minSizeLimit} باید باشد",
-                noFilesError: "هیچ فایلی آپلود نشده است",
-                onLeave: "فایل در حال آپلود است. با ترک آپلودر آپلود کنسل میشود",
-                retryFailTooManyItemsError: "تلاش مجدد آپلود بی نتیجه بود. تعداد فایل آپلود به حداکثر خود رسیده است",
-                sizeError: "حجم فایل زیاد است. حداکثر حجم فایل {sizeLimit} میتواند باشد",
-                tooManyItemsError: "محدودیت در تعداد آپلود فایل. تعداد فایل مجاز برای آپلود {itemLimit} است",
-                typeError: "نوع فایل آپلود غیر مجاز است. پسوندهای فایل مجاز قابل آپلود {extensions} میباشد",
-                unsupportedBrowserIos8Safari: "خطا در آپلود. لطفا به جای مرورگر ios8safari از ios8chrome استفاده نمایید"
-            },
-            callbacks: {
-                onUploadChunk: function (id, name, chunkData) {
-
-                },
-                onSubmitted: function (id, fileName) {
-                    fileNameList.push(fileName);
-                },
-                onValidate: function (fileData) {
-                    return fileNameList.indexOf(fileData.name) < 0;
-                },
-                onError: function (id, name, errorReason, xhr) {
-                    // UtilModal.open("خطا", errorReason, "", true, "");
-                },
-                onSubmit: onSubmit,
-                onStatusChange: onStatusChange,
-            }
-        }
-    });
-
-    // uploader.setCustomHeaders({ Authorization: "Basic 999jaGFkbWluOkNieWxjZTY3" })
-
-    //تعریف متغیر state آپلودر
-    const [uploader, setUploader] = useState(linitialStateUploader);
-
+    //مقداردهی اولیه استیت
     let initialStateFileData = {
-        "openModal": false,
-        "urlBase":props.urlBase,
+        "modalIsOpened": false,
+        "urlBase": props.urlBase,
         "customClass": props.customClass,
         "customStyle": props.customStyle,
         "type": props.type,
@@ -170,68 +41,140 @@ export default function Uploader(props) {
         "title": props.title,
         "label": props.label,
         "objectList": props.objectList,
-        "uploader": uploader,
+        "uploader": null,
         "objectSelectedList": [],
         "submitFileDataObject": {}
     };
-    //تعریف متغیر state آپلودر
-    const [fileData, setFileData] = useState(initialStateFileData);
 
-    // useEffect(() => {
-    //     let stateFileData = initialStateFileData;
-    //     setFileData(stateFileData);
-    // }, [props]);
+    //event listener callback cannot access the latest state. we use useRef to do it.
+    const [fileData, _setFileData] = useState(initialStateFileData);
+    const fileDataRef = useRef(fileData);
+    const setFileData = (data) => {
+        fileDataRef.current = data;
+        _setFileData(data);
+    };
 
+
+    // ایجاد انجین آپلودر در اولین باز شدن فرم
     useEffect(() => {
-        props.onChange(fileData);
-    },[fileData]);
+        //انجین آپلودر اولیه
+        //تعریف متغیر state آپلودر
+        fileData["uploader"] = getNewUploader(props.subSystem, props.entity, props.type, props.validationExtensionList, props.validationSizeLimit, props.validationItemLimit, [], onStatusChange);
+        setFileData({
+            ...fileData
+        });
+    }, []);
 
 
-    const onOpenModal = () => {
-        let newList = [];
-        if (props.objectList !== undefined) {
-            props.objectList.map((i) => {
+    //وضعیتها
+    //"SUBMIT": وقتی که یک فایل برای اضافه شدن به آپلودر انتخاب میشود
+    //"SUBMITTED": وقتی که یک فایل به آپلودر اضافه شود
+    //"SUBMIT_DELETE": وقتی که یک فایل برای حذف از آپلودر انتخاب شود
+    //"RESUME": وقتی که ادامه آپلود یک فایل در آپلودر انجام شود
+    //"MANUAL_RETRY": وقتی که ادامه آپلود یک فایل در آپلودر به صورت دستی توسط کاربر انجام شود
+    //"AUTO_RETRY": وقتی که ادامه آپلود یک فایل در آپلودر به صورت اتوماتیک انجام شود
+    //"CANCEL": وقتی که یک فایل در آپلودر کنسل شود
+    //"COMPLETE": وقتی که یکی از آپلود فایلها کامل گردد
+    const onStatusChange = (id) => {
+        let fileData = fileDataRef.current;
+        const {uploader} = fileData;
+        if (uploader !== null) {
+            let uploadComplete = false;
+            uploader.methods.getUploads().map(obj=> {
+                let fileObject = JSON.parse(JSON.stringify(obj));
+                if (fileObject.id === id) {
+                    fileObject["fileData"] = uploader.methods.getFile(id);
+                }
+                if (fileObject.status === statusEnum.UPLOAD_SUCCESSFUL) {
+                    if (fileObject.id === id) {
+                        if (fileObject.statusEnum !== statusEnum.DELETED) {
+                            fileData["objectSelectedList"].push(fileObject);
+                            uploadComplete = true;
+                        }
+                    }
+                }
+            });
+            if (uploadComplete) {
+                setFileData({
+                    ...fileData,
+                    "objectSelectedList": fileData["objectSelectedList"],
+                    "modalIsOpened": true
+                });
+            }
+        }
+    };
+    //متد باز کننده مدال انتخاب فایل جدید
+    const onModalOpen = () => {
+        //به دست آوردن تعداد فایلهای آپلود شده فعلی
+        let uploadedCount = 0;
+        if (fileData.objectList !== undefined) {
+            fileData.objectList.map(i => {
                 if (i.statusEnum !== "DELETED") {
-                    newList.push(i);
+                    uploadedCount++
                 }
             });
         }
-        if (props.validationItemLimit > newList.length) {
-            fileData["openModal"] = true;
+        if (props.validationItemLimit > uploadedCount) {
             setFileData({
-                ...fileData
+                ...fileData,
+                "modalIsOpened": true
             });
         } else {
             alert("خطا", " شما فقط " + props.validationItemLimit + " فایل می توانید آپلود نمائید ")
         }
     };
 
+    const onModalClose = () => {
+        setFileData({
+            ...fileData,
+            "modalIsOpened": false
+        });
+    };
 
-    const onAdd = () => {
-        if (fileData.objectSelectedList !== undefined && Object.keys(fileData.objectSelectedList).length !== 0) {
-            let changeObjectList = [];
-            fileData.objectSelectedList.forEach(function (object, index) {
+    //متد بسته شدن مدال و اضافه کننده فایلهای انتخاب شده به لیست انتخابی
+    const onModalAddButton = () => {
+        let uploadComplete = false;
+        let objectList = JSON.parse(JSON.stringify(fileData["objectList"]));
+        if (fileData.objectSelectedList !== undefined && fileData.objectSelectedList.length !== 0) {
+            fileData.objectSelectedList.map(object => {
                 if (object.status === statusEnum.UPLOAD_SUCCESSFUL) {
                     let fileViewModel = {
-
                         key: object.uuid,
                         statusEnum: statusEnum.ADDED
                     };
-                    fileData["objectList"].push(fileViewModel);
-                    changeObjectList.push(fileViewModel);
-                    setFileData({
-                        ...fileData
-                    });
+                    objectList.push(fileViewModel);
+                    uploadComplete = true;
                 }
             });
-            onStatusChange(fileData, changeObjectList, changeActionEnum.ADD, false);
         }
+        if (uploadComplete) {
+            setFileData({
+                ...fileData,
+                "objectList": objectList,
+                "modalIsOpened": false
+            });
+        }
+
     };
 
-    const onDelete = (state, fileViewModel) => {
-        var changeObjectList = [];
-        changeObjectList.push(fileViewModel);
-        onStatusChange(state.objectList, changeObjectList, changeActionEnum.DELETE);
+    useEffect(() => {
+        if (fileData.objectList !== undefined && fileData.objectList.length !== 0) {
+            if (props.onChange !== undefined) {
+                props.onChange(fileData.objectList);
+            }
+        }
+    }, [fileData.objectList]);
+//******************************
+
+
+    // uploader.setCustomHeaders({ Authorization: "Basic 999jaGFkbWluOkNieWxjZTY3" })
+
+
+    const onDelete = (objectList) => {
+        setFileData({
+            ...fileData,
+            "objectList": objectList
+        });
     };
 
     const fileInputChildren = <span>انتخاب فایل...</span>;
@@ -239,47 +182,43 @@ export default function Uploader(props) {
     const dropZoneStyle = {border: "1px dotted", height: "200px", width: "100%"};
 
     const viewUploader = () => {
-        switch (fileData.type) {
-            case typeEnum.SINGLE:
-                return (<Gallery fileInput-children={fileInputChildren} dropzone-content={dropZoneContent}
-                                 dropzone-style={dropZoneStyle}
-                                 uploader={fileData.uploader}/>);
-            case typeEnum.MULTI:
-                return (<Gallery fileInput-children={fileInputChildren} dropzone-content={dropZoneContent}
-                                 dropzone-style={dropZoneStyle}
-                                 uploader={fileData.uploader}/>);
-            default:
-                return (<span>Invalid type : {fileData.type}</span>);
+        if (fileData.uploader !== null) {
+            switch (fileData.type) {
+                case typeEnum.SINGLE:
+                    return (<Gallery fileInput-children={fileInputChildren} dropzone-content={dropZoneContent}
+                                     dropzone-style={dropZoneStyle}
+                                     uploader={fileData.uploader}/>);
+                case typeEnum.MULTI:
+                    return (<Gallery fileInput-children={fileInputChildren} dropzone-content={dropZoneContent}
+                                     dropzone-style={dropZoneStyle}
+                                     uploader={fileData.uploader}/>);
+                default:
+                    return (<span>Invalid type : {fileData.type}</span>);
+            }
         }
     };
 
-    const handleClose = () => {
-        fileData["openModal"] = false;
-        setFileData({
-            ...fileData
-        });
-    };
 
     let body = (
         <div className={classes.modalBodyUploader}>
-            <CloseIcon onClick={handleClose} className={classes.closeButtonError}/>
+            <CloseIcon onClick={onModalClose} className={classes.closeButtonError}/>
             <h3 id="simple-modal-title">{"آپلودر"}</h3>
             {viewUploader()}
-            <Button onClick={onAdd} type="submit" variant="contained" color="primary">
+            <Button onClick={onModalAddButton} type="submit" variant="contained" color="primary">
                 {"تایید"}
             </Button>
         </div>
     );
 
 
-    if (fileData.openModal) {
-
+    if (fileData.modalIsOpened) {
         return (
             <div>
                 <Modal
                     className={classes.modal}
-                    open={fileData.openModal}
-                    onClose={handleClose}
+                    open={fileData.modalIsOpened}
+                    onClose={onModalClose}
+                    disableBackdropClick={true}
                     aria-labelledby="simple-modal-title"
                     aria-describedby="simple-modal-description"
                 >
@@ -289,20 +228,29 @@ export default function Uploader(props) {
         );
 
     } else {
-        return (
-            <React.Fragment>
-                <Button onClick={onOpenModal} type="submit" variant="outlined">
-                    {"بارگذاری فایل"}
-                </Button>
-                <UploaderFileView key={Math.random()} objectList={fileData.objectList} urlBase={fileData.urlBase}
-                                  hasDownload={fileData.hasDownload} hasView={fileData.hasView}
-                                  hasDelete={fileData.hasDelete}
-                                  onChange={onDelete}/>
-            </React.Fragment>
-        )
-
+        if (fileData.objectList !== undefined && Object.keys(fileData.objectList).length !== 0) {
+            return (
+                <React.Fragment>
+                    <Button onClick={onModalOpen} type="submit" variant="outlined">
+                        {"بارگذاری فایل"}
+                    </Button>
+                    <UploaderFileView key={Math.random()}
+                                      objectList={fileData.objectList} urlBase={fileData.urlBase}
+                                      hasDownload={fileData.hasDownload} hasView={fileData.hasView}
+                                      hasDelete={fileData.hasDelete}
+                                      onChange={onDelete}/>
+                </React.Fragment>
+            )
+        } else {
+            return (
+                <React.Fragment>
+                    <Button onClick={onModalOpen} type="submit" variant="outlined">
+                        {"بارگذاری فایل"}
+                    </Button>
+                </React.Fragment>
+            )
+        }
     }
-
 }
 
 
